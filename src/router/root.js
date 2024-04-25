d8fa058b52a0dccda498f342d23984ec86e7d06f
wndudrla1011 <wndudrla1011@naver.com>
2024-04-25 18:08:05 +0900

    removed import dayRouter

--- src/router/root.js
+++ src/router/root.js
@@ -1,6 +1,5 @@
 import { Suspense, lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
-import dayRouter from "./dayRouter";
 
 const Day = lazy(() => import("../pages/Day"));
 const Main = lazy(() => import("../pages/Main"));
