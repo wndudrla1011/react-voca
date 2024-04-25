c8e67e4ca879be38b23ad6fb674a777cac829119
wndudrla1011 <wndudrla1011@naver.com>
2024-04-25 17:55:46 +0900

    added Main page

---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BasicLayout from "../layouts/BasicLayout";
+
+function Main() {
+  return (
+    <div className="Home">
+      <BasicLayout />
+    </div>
+  );
+}
+
+export default Main;
--- src/router/root.js
+++ src/router/root.js
@@ -3,13 +3,14 @@ import { createBrowserRouter } from "react-router-dom";
 import dayRouter from "./dayRouter";
 
 const Day = lazy(() => import("../pages/Day"));
+const Main = lazy(() => import("../pages/Main"));
 
 const root = createBrowserRouter([
   {
     path: "",
     element: (
       <Suspense>
-        <Day />
+        <Main />
       </Suspense>
     ),
   },
