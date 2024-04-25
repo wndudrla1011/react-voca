918950746aa65cbc39dc44ed2e0ce43c48612088
wndudrla1011 <wndudrla1011@naver.com>
2024-04-25 17:45:18 +0900

    created Header

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>
+        <a href="/">토익 영단어</a>
+      </h1>
+      <div className="menu">
+        <a href="#x" className="link">
+          단어 추가
+        </a>
+        <a href="#x" className="link">
+          Day 추가
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
