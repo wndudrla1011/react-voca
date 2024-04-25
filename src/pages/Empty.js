fdca2d8d76bf7675d0f0d7d881922a84c9598a8f
wndudrla1011 <wndudrla1011@naver.com>
2024-04-25 18:18:08 +0900

    designed back button

--- src/pages/Empty.js
+++ src/pages/Empty.js
@@ -5,7 +5,9 @@ export default function Empty() {
   return (
     <>
       <h2>잘못된 접근입니다.</h2>
-      <Link to={"/"}>돌아가기</Link>
+      <div className="back-button">
+        <Link to={"/"}>돌아가기</Link>
+      </div>
     </>
   );
 }
